147c989741edeb45a1f133f35756bd7dcc5fe03a
David <yin@yang.net>
2019-03-31 22:59:28 +0300

    keras_model_ckpt bug fix

--- vision/models/keras_models.py
+++ vision/models/keras_models.py
@@ -5,14 +5,10 @@ from fenwicks.io import *
 def keras_model_ckpt(model_class, model_dir:str, include_top:bool=False):
   create_clean_dir(model_dir)
   model = model_class(include_top=include_top)
-  model.compile(tf.train.AdamOptimizer(), 'categorical_crossentropy')
+  model.compile(tf.train.GradientDescentOptimizer(0.1),
+    'categorical_crossentropy')
   est = tf.keras.estimator.model_to_estimator(
     keras_model=model, model_dir=model_dir)
-
-  for f in tf.gfile.Glob(os.path.join(model_dir, 'keras/*')):
-    tf.gfile.Rename(f, os.path.join(model_dir, os.path.basename(f)))
-
-  tf.gfile.DeleteRecursively(os.path.join(model_dir, 'keras'))
 
 def InceptionResNetV2_ckpt(model_dir:str, include_top:bool=False):
   keras_model_ckpt(tf.keras.applications.inception_resnet_v2.InceptionResNetV2,

5f3531424ab92616b7a19774beefd65220d251e9
David <yin@yang.net>
2019-04-01 00:47:11 +0300

    tpuestimator helpers

--- utils/colab_tpu.py
+++ utils/colab_tpu.py
@@ -10,3 +10,29 @@ def setup_gcs():
     with open('/content/adc.json', 'r') as f:
       auth_info = json.load(f)
     tf.contrib.cloud.configure_gcs(sess, credentials=auth_info)
+
+def get_tpu_estimator(n_trn, n_val, model_fn, model_dir,
+  ws_dir, ws_vars, trn_bs=128, val_bs=None):
+
+  steps_per_epoch = n_trn // trn_bs
+  if val_bs is None:
+    val_bs = n_val
+
+  cluster = tf.contrib.cluster_resolver.TPUClusterResolver(TPU_ADDRESS)
+
+  tpu_cfg = tf.contrib.tpu.TPUConfig(
+    iterations_per_loop=steps_per_epoch,
+    per_host_input_for_training=tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2)
+
+  trn_cfg = tf.contrib.tpu.RunConfig(
+    cluster=cluster, model_dir=model_dir, tpu_config=tpu_cfg)
+
+  ws = tf.estimator.WarmStartSettings(
+    ckpt_to_initialize_from=ws_dir,
+    vars_to_warm_start=ws_vars)
+
+  return tf.contrib.tpu.TPUEstimator(
+    model_fn=model_fn, model_dir=model_dir,
+    train_batch_size=trn_bs, eval_batch_size=val_bs,
+    config=trn_cfg, warm_start_from=ws)
+    
--- vision/models/keras_models.py
+++ vision/models/keras_models.py
@@ -1,15 +1,35 @@
 import tensorflow as tf
 import os
 from fenwicks.io import *
+from typing import List, Tuple
 
-def keras_model_ckpt(model_class, model_dir:str, include_top:bool=False):
+def keras_model_ckpt(model_class, model_dir:str,
+  include_top:bool=False)->Tuple[str, str]:
+
   create_clean_dir(model_dir)
   model = model_class(include_top=include_top)
   model.compile(tf.train.GradientDescentOptimizer(0.1),
     'categorical_crossentropy')
-  est = tf.keras.estimator.model_to_estimator(
-    keras_model=model, model_dir=model_dir)
+  tf.keras.estimator.model_to_estimator(keras_model=model, model_dir=model_dir)
 
-def InceptionResNetV2_ckpt(model_dir:str, include_top:bool=False):
-  keras_model_ckpt(tf.keras.applications.inception_resnet_v2.InceptionResNetV2,
+  ws_dir = os.path.join(model_dir, 'keras')
+  ws_ckpt_fn = os.path.join(ws_dir, 'keras_model.ckpt')
+  return ws_dir, ws_ckpt_fn
+
+def InceptionResNetV2_ckpt(model_dir:str,
+  include_top:bool=False)->Tuple[str, str]:
+
+  return keras_model_ckpt(
+    tf.keras.applications.inception_resnet_v2.InceptionResNetV2,
     model_dir=model_dir, include_top=include_top)
+
+def get_ws_vars(ws_ckpt_fn:str)->List[str]:
+  reader = tf.train.NewCheckpointReader(ws_ckpt_fn)
+  var_to_shape_map = reader.get_variable_to_shape_map()
+  ws_vars = list(var_to_shape_map.keys())
+
+  n = len(ws_vars)
+  for i in range(n):
+    ws_vars[i] = ws_vars[i] + '[^/]'
+
+  return ws_vars

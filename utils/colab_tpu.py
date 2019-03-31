7a525b0bde6c16b6a50c6a166705d4ac7e1013b0
David <yin@yang.net>
2019-04-01 01:01:58 +0300

    get_InceptionResNetV2

--- utils/colab_tpu.py
+++ utils/colab_tpu.py
@@ -35,4 +35,28 @@ def get_tpu_estimator(n_trn, n_val, model_fn, model_dir,
     model_fn=model_fn, model_dir=model_dir,
     train_batch_size=trn_bs, eval_batch_size=val_bs,
     config=trn_cfg, warm_start_from=ws)
-    
+
+def get_clf_model_fn(model_arch, opt):
+  def model_fn(features, labels, mode, params):
+    phase = 1 if mode == tf.estimator.ModeKeys.TRAIN else 0
+    tf.keras.backend.set_learning_phase(phase)
+
+    model = model_arch()
+    logits = model(features)
+
+    loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
+    step = tf.train.get_or_create_global_step()
+
+    opt = tf.contrib.tpu.CrossShardOptimizer(opt)
+    with tf.control_dependencies(model.get_updates_for(features)):
+      train_op = opt.minimize(loss, global_step=step)
+
+    classes = tf.math.argmax(logits, axis=-1)
+    metric_fn = lambda classes, labels: {'accuracy':
+                                         tf.metrics.accuracy(classes, labels)}
+    tpu_metrics = (metric_fn, [classes, labels])
+
+    return tf.contrib.tpu.TPUEstimatorSpec(mode, loss=loss, train_op=train_op,
+                                             eval_metrics = tpu_metrics)
+
+  return model_fn    

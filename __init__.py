fd52eaed7b735ed32071e5610a2ef9045e78ce7b
David <yin@yang.net>
2019-04-05 12:31:58 +0300

    import bug fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-import datasets
-import io
-from utils import colab_tpu
-import tpu_estimator
-import layers
-import train
+from . import datasets
+from . import io
+from .utils import colab_tpu
+from . import tpu_estimator
+from . import layers
+from . import train
 from .vision.models import keras_models

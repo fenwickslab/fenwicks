0ae166d70685943b31fa94a37942e38d1a5e87bb
David <yin@yang.net>
2019-04-10 13:37:17 +0800

    Update layers.py

--- layers.py
+++ layers.py
@@ -26,26 +26,20 @@ class GlobalPools(tf.keras.Model):
         return tf.keras.layers.concatenate([self.gmp(x), self.gap(x)])
 
 
-class DenseBlk(tf.keras.Model):
+class DenseBlk(Sequential):
     def __init__(self, c: int, drop_rate: float):
         super().__init__()
-        self.dense = tf.keras.layers.Dense(c, use_bias=False)
-        self.bn = tf.keras.layers.BatchNormalization()
-        self.dropout = tf.keras.layers.Dropout(drop_rate)
-
-    def call(self, x):
-        return self.dropout(tf.nn.relu(self.bn(self.dense(x))))
+        self.add(tf.keras.layers.Dense(c, use_bias=False))
+        self.add(tf.keras.layers.BatchNormalization())
+        self.add(tf.keras.layers.Dropout(drop_rate))
 
 
-class ConvBN(tf.keras.Model):
+class ConvBN(Sequential):
     def __init__(self, c: int, kernel_size=3, kernel_initializer='glorot_uniform', bn_mom=0.99, bn_eps=0.001):
         super().__init__()
-        self.conv = tf.keras.layers.Conv2D(filters=c, kernel_size=kernel_size, kernel_initializer=kernel_initializer,
-                                           padding='same', use_bias=False)
-        self.bn = tf.keras.layers.BatchNormalization(momentum=bn_mom, epsilon=bn_eps)
-
-    def call(self, x):
-        return tf.nn.relu(self.bn(self.conv(x)))
+        self.add(tf.keras.layers.Conv2D(filters=c, kernel_size=kernel_size, kernel_initializer=kernel_initializer,
+                                        padding='same', use_bias=False))
+        self.add(tf.keras.layers.BatchNormalization(momentum=bn_mom, epsilon=bn_eps))
 
 
 class ConvBlk(Sequential):

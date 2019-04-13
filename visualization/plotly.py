75e70778752da3fb9fbafd4ee8798cea95172dff
Yin Yang <yin@yang.net>
2019-04-13 21:48:58 +0300

    plot lr func

--- visualization/plotly.py
+++ visualization/plotly.py
@@ -1,4 +1,8 @@
 import IPython
+import tensorflow as tf
+import plotly.plotly
+import plotly.graph_objs as go
+import numpy as np
 
 
 def configure_plotly_browser_state():
@@ -15,5 +19,50 @@ def configure_plotly_browser_state():
     '''))
 
 
-def configure_plotly_browser_state_all_cells():
+def init_plotly():
+    tf.enable_eager_execution()
+    plotly.offline.init_notebook_mode(connected=True)
     IPython.get_ipython().events.register('pre_run_cell', configure_plotly_browser_state)
+
+
+def simulate_lr_func(lr_func, step, total_steps):
+    lr_values = np.zeros(total_steps)
+
+    for i in range(total_steps):
+        step.assign(i)
+        lr = lr_func()
+        lr_values[i] = lr.numpy()
+        step.assign(i)
+
+    return lr_values
+
+
+def plot_lr_func(lr_func, total_steps):
+    step = tf.train.get_or_create_global_step()
+    trace = go.Scatter(
+        y=simulate_lr_func(lr_func, step, total_steps),
+    )
+
+    data = [trace]
+
+    layout = go.Layout(
+        autosize=False,
+        width=350,
+        height=350,
+        yaxis=go.layout.YAxis(
+            title='Learning rate',
+        ),
+        xaxis=go.layout.XAxis(
+            title='Global step',
+        ),
+        margin=go.layout.Margin(
+            l=80,
+            r=20,
+            b=40,
+            t=20,
+        ),
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    plotly.offline.iplot(fig)

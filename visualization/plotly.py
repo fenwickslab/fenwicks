e2b06163bbb997db6aa9bce449dd2ab1600ca4a4
Yin Yang <yin@yang.net>
2019-04-14 09:09:34 +0300

    Update plotly.py

--- visualization/plotly.py
+++ visualization/plotly.py
@@ -25,23 +25,21 @@ def setup():
     IPython.get_ipython().events.register('pre_run_cell', configure_plotly_browser_state)
 
 
-def simulate_lr_func(lr_func, step, total_steps):
+def simulate_lr_func(lr_func, total_steps):
     lr_values = np.zeros(total_steps)
-
-    for i in range(total_steps):
-        step.assign(i)
-        lr = lr_func()
-        lr_values[i] = lr
+    step = tf.placeholder(tf.int64)
+    lr = lr_func(step=step)
 
     with tf.Session() as sess:
-        x = sess.run(lr_values)
+        sess.run(tf.global_variables_initializer())
+        for i in range(total_steps):
+            lr_values[i] = sess.run(lr, feed_dict={step: i})
 
-    return x
+    return lr_values
 
 
 def plot_lr_func(lr_func, total_steps):
-    step = tf.train.get_or_create_global_step()
-    trace = go.Scatter(y=simulate_lr_func(lr_func, step, total_steps))
+    trace = go.Scatter(y=simulate_lr_func(lr_func, total_steps))
     data = [trace]
     layout = go.Layout(autosize=False, width=350, height=350, yaxis=go.layout.YAxis(title='Learning rate'),
                        xaxis=go.layout.XAxis(title='Training step'), margin=go.layout.Margin(l=80, r=20, b=40, t=20))

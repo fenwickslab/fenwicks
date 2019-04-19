ffe5c150bf01f23442a684823040a32823fca96b
David <yin@yang.net>
2019-04-19 23:51:54 +0300

    Update anim.py

--- visualization/anim.py
+++ visualization/anim.py
@@ -19,7 +19,7 @@ def show_images(X):
     fig.set_size_inches(3, 3)
     fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
     ax.axis('off')
-    anim = animation.FuncAnimation(fig, animate, frames=4, interval=1000)
+    anim = animation.FuncAnimation(fig, animate, frames=len(X), interval=1000)
     display(anim)
 
 
@@ -41,7 +41,7 @@ def show_dataset(ds: tf.data.Dataset, num_batch: int = 1, n_img: int = 10):
             if len(x) >= n_img:
                 X.extend(x[:n_img])
                 break
-            X.extend(x)
+            X.extend(x / 2 + 0.5)  # fixme
             n_img -= len(x)
 
     show_images(X)

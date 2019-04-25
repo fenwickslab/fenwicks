99684bc820e45c5a722b6717e3c7adcdfb6a0cb0
Yin Yang <yin@yang.net>
2019-04-25 19:35:27 +0300

    Update transform.py

--- vision/transform.py
+++ vision/transform.py
@@ -53,6 +53,7 @@ def random_lighting(x: tf.Tensor, max_lighting: float = 0.2) -> tf.Tensor:
     x = tf.image.random_brightness(x, 0.5 * max_lighting)
     x = tf.clip_by_value(x, 0.0, 1.0)
     x = tf.image.random_contrast(x, 1 - max_lighting, 1 / (1 - max_lighting))
+    x = tf.clip_by_value(x, 0.0, 1.0)
     return x
 
 

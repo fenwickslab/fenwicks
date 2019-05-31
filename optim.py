15f7ce78c5ea467c7753c9e44bbffbccb75dc22e
David <yin@yang.net>
2019-05-31 18:26:48 +0300

    Update optim.py

--- optim.py
+++ optim.py
@@ -30,12 +30,11 @@ class Adam(tf.train.AdamOptimizer):
     def __init__(self, lr: Union[float, tf.Tensor] = 0.001, wd: float = None, beta1: float = 0.9, beta2: float = 0.999,
                  epsilon: float = 1e-8, exclude_from_wd: List[str] = None, clip_norm: float = None):
         super().__init__(learning_rate=lr, beta1=beta1, beta2=beta2, epsilon=epsilon)
-        self.wd = 1 - wd * lr if wd else None
+        self.wd = 1 - wd * lr if wd and wd > 0 else None
         self.exclude_from_wd = exclude_from_wd
         self.clip_norm = clip_norm
 
-    def compute_gradients(self, loss: tf.Tensor, var_list: List[tf.Tensor] = None, **kwargs) -> Union[
-        List[Tuple], Iterator[Tuple]]:
+    def compute_gradients(self, loss: tf.Tensor, var_list: List[tf.Tensor] = None, **kwargs) -> Iterator[Tuple]:
 
         grads_and_vars = super().compute_gradients(loss, var_list)
         if not self.clip_norm and not self.wd:
@@ -45,18 +44,29 @@ class Adam(tf.train.AdamOptimizer):
         if self.clip_norm is not None:
             gs, _ = tf.clip_by_global_norm(gs, clip_norm=self.clip_norm)
 
-        if self.wd is None:
-            return zip(gs, vs)
+        return zip(gs, vs)
 
-        grads_and_vars = []
-        for g, v in zip(gs, vs):
-            if self._do_use_wd(get_variable_name(v.name)):
-                v *= self.wd
-            grads_and_vars.append((g, v))
+    def _apply_dense(self, grad, var):
+        if not self._do_use_wd(get_variable_name(var.name)):
+            return super()._apply_dense(grad, var)
 
-        return grads_and_vars
+        m = self.get_slot(var, "m")
+        v = self.get_slot(var, "v")
+        beta1_power, beta2_power = self._get_beta_accumulators()
+
+        lr = (self._lr_t * tf.sqrt(1 - beta2_power) / (1 - beta1_power))  # learning rate bias correction
+
+        m_t = tf.assign(m, m * self._beta1_t + grad * (1 - self._beta1_t))
+        v_t = tf.assign(v, v * self._beta2_t + tf.square(grad) * (1 - self._beta2_t))
+        v_sqrt = tf.sqrt(v_t)
+
+        var_update = tf.assign(var, var * self.wd - lr * m_t / (v_sqrt + self._epsilon_t))
+        return tf.group(*[var_update, m_t, v_t])
 
     def _do_use_wd(self, param_name: str) -> bool:
+        if self.wd is None:
+            return False
+
         if self.exclude_from_wd:
             for r in self.exclude_from_wd:
                 if re.search(r, param_name) is not None:

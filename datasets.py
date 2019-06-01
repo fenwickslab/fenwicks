b2c9964dfd81b5fa435bc7b5bdbe4e00b13f21e8
David <yin@yang.net>
2019-06-01 12:40:15 +0300

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -30,12 +30,16 @@ class URLs:
     KAGGLE_BONE_AGE = f'{KAGGLE_DATASETS_DOWNLOAD}kmader/rsna-bone-age'
     KAGGLE_PNEUMONIA = f'{KAGGLE_DATASETS_DOWNLOAD}paultimothymooney/chest-xray-pneumonia'
 
-    GLUE = 'https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data'
-    GLUE_COLA = f'{GLUE}%2FCoLA.zip?alt=media&token=46d5e637-3411-4188-bc44-5809b5bfb5f4'
+    GLUE = 'https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2F'
+    GLUE_COLA = f'{GLUE}CoLA.zip?alt=media&token=46d5e637-3411-4188-bc44-5809b5bfb5f4'
+    GLUE_MRPC = f'{GLUE}mrpc_dev_ids.tsv?alt=media&token=ec5c0836-31d5-48f4-b431-7480817f1adc'
+
+    MRPC_TRAIN = 'https://dl.fbaipublicfiles.com/senteval/senteval_data/msr_paraphrase_train.txt'
+    MRPC_TEST = 'https://dl.fbaipublicfiles.com/senteval/senteval_data/msr_paraphrase_test.txt'
 
 
 def untar_data(url: str, dest: str = '.') -> str:
-    if not os.path.isdir(dest):
+    if not gfile.isdir(dest):
         gfile.makedirs(dest)
     url_path = urlparse(url).path
     fn = os.path.basename(url_path)

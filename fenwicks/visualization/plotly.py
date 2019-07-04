99eb33d752bdbb9d3747cfc6b3702f18bf458ae7
David <yin@yang.net>
2019-07-04 17:05:33 +0800

    Update plotly.py

--- fenwicks/visualization/plotly.py
+++ fenwicks/visualization/plotly.py
@@ -175,7 +175,17 @@ def show(fig: go.Figure):
     plotly.offline.iplot(fig)
 
 
-def plot_kde(s: pd.Series, h: int = 300, w: int = 350, show_fig=True) -> go.Figure:
+def trace_kde(s: pd.Series) -> go.Scatter:
+    x = s.dropna()
+    bw = bandwidths.bw_scott(x)
+    x_plot = np.linspace(x.min(), x.max(), 1000)[:, np.newaxis]
+    x = np.array(x)[:, np.newaxis]
+    kde = KernelDensity(bandwidth=bw).fit(x)
+    log_dens = kde.score_samples(x_plot)
+    return go.Scatter(x=x_plot[:, 0], y=np.exp(log_dens), fill='tozeroy', line=dict(color='#AAAAFF'))
+
+
+def plot_kde(s: pd.Series, h: int = 300, w: int = 350):
     x = s.dropna()
     bw = bandwidths.bw_scott(x)
     x_plot = np.linspace(x.min(), x.max(), 1000)[:, np.newaxis]
@@ -184,10 +194,7 @@ def plot_kde(s: pd.Series, h: int = 300, w: int = 350, show_fig=True) -> go.Figu
     log_dens = kde.score_samples(x_plot)
     trace = go.Scatter(x=x_plot[:, 0], y=np.exp(log_dens), fill='tozeroy', line=dict(color='#AAAAFF'))
 
-    if show_fig:
-        layout = go.Layout()
-        layout_size_margin(layout, h, w, l=20, r=0, b=20, t=0)
-        fig = go.Figure(data=[trace], layout=layout)
-        show(fig)
-
-    return trace
+    layout = go.Layout()
+    layout_size_margin(layout, h, w, l=20, r=0, b=20, t=0)
+    fig = go.Figure(data=[trace], layout=layout)
+    show(fig)

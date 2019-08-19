f375871f57d1e8d64be1f2fc19ffbaa51cddd872
yin@yang.net <7Pqd&oL7>
2019-08-19 12:58:10 +0300

    Update test_tf_ckpt.py

--- test/test_tf_ckpt.py
+++ test/test_tf_ckpt.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import fenwicks as fw
+import numpy as np
+
+from typing import Dict
 
 
 def setup_one_var_ckpt(model_name: str = 'my-model', global_step: int = 1):
@@ -59,6 +62,103 @@ def test_ckpt_read():
     remove_ckpt('my-model', 1)
 
 
-def test_keras_ckpt_vgg16():
+def test_keras_vgg16_ckpt():
     fw.keras_models.get_model('VGG16', root_dir='.')
     tf.io.gfile.rmtree('./model')
+
+
+def count_vars(var_to_shape_map: Dict):
+    n_weight = 0
+    n_var = 0
+    for k, v in var_to_shape_map.items():
+        if k != 'global_step':
+            # print(k, ':', v, ',', np.prod(v))
+            n_weight += np.prod(v)
+        n_var += 1
+    return n_weight, n_var
+
+
+def check_keras_model_num_vars(model_name: str, expected_n_weight: int = -1):
+    fw.keras_models.get_model(model_name, root_dir='.')
+
+    ckpt_fn = f'./model/{model_name}/keras/keras_model.ckpt'
+    reader = tf.train.NewCheckpointReader(ckpt_fn)
+    var_to_shape_map = reader.get_variable_to_shape_map()
+    n_weight, n_var = count_vars(var_to_shape_map)
+    if expected_n_weight > 0:
+        assert n_weight == expected_n_weight
+    else:
+        print('Total number of variables in checkpoint:', n_weight)
+
+    tf.io.gfile.rmtree('./model')
+
+
+def test_keras_vgg16_num_vars():
+    check_keras_model_num_vars('VGG16', 14714688)
+
+
+def test_keras_vgg19_num_vars():
+    check_keras_model_num_vars('VGG19', 20024384)
+
+
+def test_keras_resnet50_num_vars():
+    check_keras_model_num_vars('ResNet50', 23587712)
+
+
+def test_keras_resnet101_num_vars():
+    check_keras_model_num_vars('ResNet101', 42658176)
+
+
+def test_keras_resnet152_num_vars():
+    check_keras_model_num_vars('ResNet152', 58370944)
+
+
+def test_keras_resnet50v2_num_vars():
+    check_keras_model_num_vars('ResNet50V2', 23564800)
+
+
+def test_keras_resnet101v2_num_vars():
+    check_keras_model_num_vars('ResNet101V2', 42626560)
+
+
+def test_keras_resnet152v2_num_vars():
+    check_keras_model_num_vars('ResNet152V2', 58331648)
+
+
+# fixme
+def test_keras_resnext50_num_vars():
+    check_keras_model_num_vars('ResNeXt50')
+
+
+def test_keras_inceptionv3_num_vars():
+    check_keras_model_num_vars('InceptionV3', 21802784)
+
+
+def test_keras_inceptionresnetv2_num_vars():
+    check_keras_model_num_vars('InceptionResNetV2', 54336736)
+
+
+def test_keras_xception_num_vars():
+    check_keras_model_num_vars('Xception', 20861480)
+
+
+# todo: mobilenet
+
+def test_keras_densenet121_num_vars():
+    check_keras_model_num_vars('DenseNet121', 7037504)
+
+
+def test_keras_densenet169_num_vars():
+    check_keras_model_num_vars('DenseNet169', 12642880)
+
+
+def test_keras_densenet201_num_vars():
+    check_keras_model_num_vars('DenseNet201', 18321984)
+
+
+def test_keras_nasnetlarge_num_vars():
+    check_keras_model_num_vars('NASNetLarge', 84916818)
+
+
+def test_keras_nasnetmobile_num_vars():
+    check_keras_model_num_vars('NASNetMobile', 4269716)
